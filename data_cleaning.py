952d1cbe35d13329fd03835b8126a67db8b94664
jackhardwick <83730307+hathuwic@users.noreply.github.com>
2022-12-02 23:14:06 +0100

    Lots of things

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,7 +12,7 @@ if args.input is None or args.output is None:
     print("Please specify input and output paths -i --input, -o --output.")
     sys.exit(0)
 if not args.input.endswith(".csv") and not args.input.endswith(".xlsx"):
-    print("Input file should be csv or xlsx file.")
+    print("Input file should be .csv or .xlsx file.")
     sys.exit(0)
 
 input_path = args.input
@@ -27,15 +27,16 @@ except FileNotFoundError as e:
 df["date"] = pd.to_datetime(
     dict(year=df["year"], month=df["month"], day=df["day"], seconds=df["sec_of_day"])
 )
-df["cumulative_seconds"] = (df["date"] - df.iloc[0]["date"]).dt.total_seconds()
+df["cumulative_secs"] = (df["date"] - df.iloc[0]["date"]).dt.total_seconds()
 cols = [
-    "cumulative_seconds",
+    "cumulative_secs",
     "proton_density",
     "bulk_speed",
     "ion_temp",
     "bz",
     "bt",
     "longitude",
+    "kp_index"
 ]
 df = df[cols]
 
@@ -52,10 +53,12 @@ df.interpolate(method="linear", inplace=True)
 
 # 5 minutes average
 df_res = df.rolling(5, min_periods=1, step=5).mean()
-df_res["cumulative_seconds"] = (
-    df["cumulative_seconds"].rolling(5, min_periods=1, step=5).max()
-)
+df_res["cumulative_secs"] = (df["cumulative_secs"].rolling(5, min_periods=1, step=5).max())
+df_res["kp_index"] = (df["kp_index"].rolling(5, min_periods=1, step=5).min())
 df_res.reset_index(drop=True, inplace=True)
+
+# rename columns to names expected by the notebook
+df_res.rename(columns={'bulk_speed':'speed', 'longitude':'phi_angle'}, inplace=True)
 
 # save file
 df_res.to_csv(output_path, index=False)
